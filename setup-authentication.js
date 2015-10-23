141d4e767c3076be87f0dd4aea740a06b48baac6
Jason McCarrell <jason.abz@gmail.com>
2015-10-23 19:36:01 -0400

    fixes

--- setup-authentication.js
+++ setup-authentication.js
@@ -3,11 +3,14 @@ var UserAuth = require('./user-auth');
 var SetupAuthentication = function(app, userService, passport, config) {
     app.use(config.route_prefix + config.users_route, userService);
     app.service(config.route_prefix + config.users_route).before(UserAuth());
+    app.use(config.route_prefix + '/user', function(req, res) {
+        res.status(200).send(JSON.stringify(req.user));
+    });
     //TODO: understand what 'local' means and then make it configurable
     app.post(config.route_prefix + config.login_route, passport.authenticate('local'), function(req, res) {
 	    res.status(200).send(JSON.stringify(req.user));
     });
-    app.get(config.route_prefix + config.login_route, function(req, res) {
+    app.get(config.route_prefix + '/logout', function(req, res) {
         req.logout();
         res.status(200).send('{"user": null}');
     });
